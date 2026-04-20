7054093bb8845b4e842b7ec683481775113fbcc9
Collin Makwala <collinmakwala@gmail.com>
2026-04-20 14:21:40 +0200

    add customers sample data

--- database/sample_data.sql
+++ database/sample_data.sql
@@ -4,3 +4,11 @@ INSERT INTO Categories (CategoryName) VALUES
 ('Clothing'),
 ('Home & Kitchen'),
 ('Books');
+
+-- CUSTOMERS
+INSERT INTO Customers (FirstName, LastName, Email, Phone) VALUES
+('John', 'Doe', 'john.doe@email.com', '0712345678'),
+('Sarah', 'Smith', 'sarah.smith@email.com', '0723456789'),
+('Michael', 'Brown', 'michael.brown@email.com', '0734567890'),
+('Emily', 'Davis', 'emily.davis@email.com', '0745678901'),
+('David', 'Wilson', 'david.wilson@email.com', '0756789012');
